28b5a04b82faf601ccab6ab7355bf89f0a55ba6b
vmuljo <muljo@usc.edu>
2022-02-07 17:24:10 -0800

    Added part 2 code

---
+++ p2_starter.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class Person{
+    protected:
+        string name;
+        int age;
+    public:
+        Person(string n, int a){
+            name = n;
+            age = a;
+        }
+        Person(){
+            name = "";
+            age = -1;
+        }
+        virtual void print_message(){
+            cout << "---------------\n";
+            cout << "Name: " << name << "\n";
+            cout << "Age: " << age << "\n";
+        }
+};
+
+class Student: public Person{
+    private:
+        string school;
+        string major;
+    public:
+        Student(string n, int a, string s, string m){
+            name = n;
+            age = a;
+            school = s;
+            major = m;
+        }
+        void print_message(){
+            cout << "---------------\n";
+            cout << "Name: " << name << "\n";
+            cout << "Age: " << age << "\n";
+            cout << "School: " << school << "\n";
+            cout << "Major: " << major << "\n";
+        }
+};
+
+int main(){
+    
+    //Use these lines to test your code
+    Person p1("Tommy",28);
+    p1.print_message();
+
+    Student s1 = Student("Anna",22,"USC","EE");
+    Person *p2 = &s1;
+    p2->print_message();
+    
+}
